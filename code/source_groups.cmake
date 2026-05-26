6de7a91752de8476b43a76a0b0ef89158d2cc7f1
Mike Nelson <mjn.other@gmail.com>
2026-05-26 12:58:03 -0500

    Qtfred sexp tree refactor (#4)
    * long project will be long
    
    * add shared foundation files
    
    * unify some shared types across both editors
    
    * sexptreemodel framework
    
    * move opf functions to the model
    
    * move shared functions into the model
    
    * move variable/container stuff to the model
    
    * sexptreeactions and ui interface
    
    * wire the model into the ui
    
    * move some shared logic to the model
    
    * move more methods to the model
    
    * move clipboard to model
    
    * create new right click menu model
    
    * begin connecting the new right click menu
    
    * wire up the new right click menu in fred
    
    * wire up the new right click menu in qtfred
    
    * move help text and visuals to model
    
    * move menu option enable disable to model
    
    * wire up sexp_tree to the model
    
    * rename sexp_tree files to sexp_tree_ui
    
    * fix build errors
    
    * fix warning
    
    * remove all opf forwarders
    
    * remove some forwarder methods
    
    * connect qtfred events editor directly to model
    
    * wite plan for unifying tree types
    
    * replace legacy m_mode with treeflags
    
    * fix qtfred campaign dialog
    
    * fix qtfred ship/wing dialog trees
    
    * build error
    
    * documentation
    
    * more documentation
    
    * even more documentation
    
    * move more opf functions to opf file
    
    * clean up context menu function
    
    * move default argument to opf file
    
    * build errors
    
    * fix qtfred dialogs and annoations
    
    * new shared annotations model
    
    * sexp_tree_ui to sexp_tree_view
    
    * documentation pass
    
    * document annotations model
    
    * delete the plan
    
    * move event annotations to the new model
    
    * generic cleanup
    
    * remove some old defines
    
    * cleanup includes
    
    * more cleanup
    
    * parenthesis
    
    * more parenthesis
    
    * variable collisions
    
    * too many parenthesis this time
    
    * clang modernization
    
    * variable names again
    
    * more clang and assertions
    
    * static cast
    
    * clang again
    
    * fix some bugs and issues
    
    * Refactor sexp tree action duplication into SexpTreeActions
    
    * fix a few more minor issues
    
    * avoid clang false positive
    
    * rebase tweaks
    
    * fix annotations issue
    
    * fix typo and two small bugs claude found
    
    * fix event editor tree widget loading
    
    * fix ship and wing sexp tree editors in qtfred
    
    * fix qtfred briefing and debriefing trees
    
    * fix a regression Claude found
    
    * address feedback
    
    * fix annotations for root items in qtfred by copying fred's method
    
    * address feedback
    
    * rescope variables
    
    * clang

--- code/source_groups.cmake
+++ code/source_groups.cmake
@@ -848,6 +848,14 @@ add_file_folder("MissionEditor"
 	missioneditor/campaignsave.h
 	missioneditor/missionsave.cpp
 	missioneditor/missionsave.h
+	missioneditor/sexp_annotation_model.cpp
+	missioneditor/sexp_annotation_model.h
+	missioneditor/sexp_tree_model.cpp
+	missioneditor/sexp_tree_model.h
+	missioneditor/sexp_tree_opf.cpp
+	missioneditor/sexp_tree_opf.h
+	missioneditor/sexp_tree_actions.cpp
+	missioneditor/sexp_tree_actions.h
 )
 
 # MissionUI files
